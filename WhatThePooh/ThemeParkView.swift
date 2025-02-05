cd722183c984fca7ebd6aabaf88efa5170ab83de
Brent Cromley <brentlyjr@yahoo.com>
2025-02-05 09:44:01 -0800

    Updated layout view to allow for horizontal viewing.

--- WhatThePooh/ThemeParkView.swift
+++ WhatThePooh/ThemeParkView.swift
@@ -11,34 +11,32 @@ struct ThemeParkView: View {
     @StateObject private var viewModel = ThemeParkViewModel()
 
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
-                    ForEach(viewModel.entities.indices, id: \.self) { index in
-                        HStack {
-                            if index % 2 == 0 {
-                                Text(viewModel.entities[index].name)
-                                    .font(.footnote)
-                                    .lineLimit(nil)
-                                    .fixedSize(horizontal: false, vertical: true)
-                            } else {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
+                ForEach(viewModel.entities.indices, id: \.self) { index in
+                    HStack {
+                        if index % 2 == 0 {
+                            Text(viewModel.entities[index].name)
+                                .font(.footnote)
+                                .lineLimit(nil)
+                                .fixedSize(horizontal: false, vertical: true)
+                        } else {
 //                                Spacer() // Placeholder for second column content
-                                Text(viewModel.entities[index].status ?? "Unknown") // Show status
-                                    .font(.footnote)
-                                    .foregroundColor(.gray)
-                            }
+                            Text(viewModel.entities[index].status ?? "Unknown") // Show status
+                                .font(.footnote)
+                                .foregroundColor(.gray)
                         }
-                        .padding()
-                        .background(Color.blue.opacity(0.1))
-                        .cornerRadius(8)
                     }
+                    .padding()
+                    .background(Color.blue.opacity(0.1))
+                    .cornerRadius(8)
                 }
-                .padding()
             }
-            .navigationTitle("What the Pooh!")
-            .onAppear {
-                viewModel.fetchEntities(for: "7340550b-c14d-4def-80bb-acdb51d49a66")
-            }
+            .padding()
+        }
+        .navigationTitle("What the Pooh!")
+        .onAppear {
+            viewModel.fetchEntities(for: "7340550b-c14d-4def-80bb-acdb51d49a66")
         }
     }
 }
